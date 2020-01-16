02e1a20bec6c2007c95329759888b3100ea4aab2
Hot-Rod-5000 <54560641+Hot-Rod-5000@users.noreply.github.com>
2020-01-16 09:49:02 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,10 +1,10 @@
 //155E 01-14-2020
-//Rodney Cruz Andrew Socha
+//Rodney Cruz
 // A simple hello world program.
 
 #include <stdlib.h>
 #include <stdio.h>
 int main(int argc, char **argv) {
-printf("Hello World!\n");
+printf("Computer Engineer\n");
 return 0;
 }
